9c9ff186331c21409781ca98938495940011f370
moonice1 <ymoon1006@163.com>
2021-08-04 20:39:33 +0800

    add day10

---
+++ Day10-DadJokes/index.js
@@ -0,0 +1,34 @@
+const jokeE1 = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
+
+//USING ASYNC/AWAIT
+// async function generateJoke() {
+//     const config = {
+//         headers: {
+//             Accept: 'application/json'
+//         }
+//     }
+
+//     const res = await fetch('https://icanhazdadjoke.com', config)
+
+//     const data = await res.json()
+
+//     jokeE1.innerHTML = data.joke
+// }
+
+//USING  .then()
+function generateJoke() {
+    const config = {
+        headers: {
+            Accept: 'application/json'
+        }
+    }
+
+    fetch('https://icanhazdadjoke.com', config).then((res) => res.json()).then((data) => {
+        jokeE1.innerHTML = data.joke
+    })
+}

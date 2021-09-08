e6f7a3db6e947d635a6eb2fe579a2af0b7ec0336
moonice1 <ymoon1006@163.com>
2021-09-08 10:41:44 +0800

    add导入导出功能

--- Day22-DrawingApp/index.js
+++ Day22-DrawingApp/index.js
@@ -5,20 +5,28 @@ const sizeEl = document.getElementById('size')
 const colorEl = document.getElementById('color')
 const clearEl = document.getElementById('clear')
 
+const exportBtn = document.getElementById('export')
+const importBtn = document.getElementById('import')
+// 创建一个CanvasRenderingContext2D对象作为2D渲染的上下文
 const ctx = canvas.getContext('2d')
+console.log(ctx)
 
 let size = 10
 let isPressed = false
 let color = 'black'
 let x
 let y
+let data = []
 
+// event.offsetX, event.offsetY 鼠标点击的位置距离div盒子的大小
 canvas.addEventListener('mousedown', (e) => {
     isPressed = true
 
     x = e.offsetX
     y = e.offsetY
+    data.push({ xc: x, yc: y })
 })
+console.log(data)
 
 canvas.addEventListener('mouseup', (e) => {
     isPressed = false
@@ -37,9 +45,15 @@ canvas.addEventListener('mousemove', (e) => {
 
         x = x2
         y = y2
+        data.push({ xc: x, yc: y })
     }
 })
+console.log(data)
 
+//x,y始终为起点,x2,y2为终点
+
+//beginPath()开始一条路径，或重置当前的路径
+//context.arc(x,y,r,sAngle,eAngle,counterclockwise);
 function drawCircle(x, y) {
     ctx.beginPath()
     ctx.arc(x, y, size, 0, Math.PI * 2)
@@ -83,3 +97,53 @@ decreaseBtn.addEventListener('click', () => {
 colorEl.addEventListener('change', (e) => color = e.target.value)
 
 clearEl.addEventListener('click', () => ctx.clearRect(0, 0, canvas.width, canvas.height))
+
+exportBtn.addEventListener('click', () => {
+    saveShareContent(JSON.stringify(data), 'draw.json')
+})
+
+
+//保存文件
+function saveShareContent(content, fileName) {
+    let downLink = document.createElement('a')
+    downLink.download = fileName
+    //字符内容转换为blod地址
+    let blob = new Blob([content])
+    downLink.href = URL.createObjectURL(blob)
+    // 链接插入到页面
+    document.body.appendChild(downLink)
+    downLink.click()
+    // 移除下载链接
+    document.body.removeChild(downLink)
+}
+
+//导入文件
+
+document.getElementById('fileImport').addEventListener('click', () => {
+    document.getElementById('files').click()
+})
+function fileImport() {
+    var selectedFile = document.getElementById('files').files[0]
+    console.log(selectedFile)
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    var reader = new FileReader();//这是核心,读取操作就是由它完成.
+    reader.readAsText(selectedFile);//读取文件的内容,也可以读取文件的URL
+    reader.onload = function () {
+        //当读取完成后回调这个函数,然后此时文件的内容存储到了result中,直接操作即可
+        console.log(this.result);
+
+        var result = JSON.parse(this.result)
+        console.log(typeof result)
+
+        for (let i = 0; i < result.length; i++) {
+
+            if (i < result.length - 1) {
+                drawCircle(result[i + 1].xc, result[i + 1].yc)
+                drawLine(result[i].xc, result[i].yc, result[i + 1].xc, result[i + 1].yc)
+            }
+        }
+    }
+
+}
